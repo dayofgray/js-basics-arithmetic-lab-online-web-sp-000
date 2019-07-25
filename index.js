4508b90ce55c6fa7e946e44f901d0a447c0ca9c0
Grayson <graysonpberger@gmail.com>
2019-07-25 02:09:20 -0400

    Done.

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
+let var1 = 31
+let var2 = 2
+let multiply = var1 * var2
+
+let randomNumber = Math.random() * 20;
+const newID = 1000000000 + oldID;
+const ageIsValid = Number.isInteger(currentAge);
+const randomInteger = Math.floor(randomNumber);
+const randomUserID = randomInteger + 1000000001;
